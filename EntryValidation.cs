158642f503be34d68bd65f15c9802a85b67e6859
Akash Pratap Singh <akashpratap2507@gmail.com>
2020-10-07 01:03:43 +0530

    [Akash] Added firstname validation program

---
+++ EntryValidation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegularExpressions
+{
+    class EntryValidation
+    {
+        public void ValidateFirstName()
+        {
+            {
+               
+                string patternFirstName = @"^[A-Z]{1}([a-zA-Z]+){2,}";
+                Regex regex = new Regex(patternFirstName);
+              
+                Console.WriteLine("Enter the first name of user");
+                string firstName = Console.ReadLine();
+               
+                if (regex.IsMatch(firstName))
+                {
+                    Console.WriteLine("The entry for first name is valid");
+                   
+                }
+               
+                else
+                {
+                    Console.WriteLine("Invalid entry. Enter atleast 3 characters and first letter should be capital");
+                  
+                }
+            }
+        }
+    }
+}
